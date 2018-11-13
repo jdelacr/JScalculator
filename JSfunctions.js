afebf357d61af911205e83b410ae9e4654b1a2c7
Jan Justyn Dela Cruz <justyn@Jans-MacBook-Pro.local>
2018-11-13 12:48:58 -0500

    Started Continuing Calculator

--- JSfunctions.js
+++ JSfunctions.js
@@ -1,5 +1,31 @@
-document.getElementById("reset").addEventListener("click", reset);
+
+
+
 
 function reset(){
     console.log("reset has been pressed");
 }
+
+function start(){
+    document.getElementById("reset").addEventListener("click", reset,false);
+    document.getElementById("Subtract").addEventListener("click", subtract,false);
+    document.getElementById("Addition").addEventListener("click", reset,false);
+    document.getElementById("Multiply").addEventListener("click", reset,false);
+    document.getElementById("Division").addEventListener("click", reset,false);
+    document.getElementById("Equal").addEventListener("click", reset,false);
+    document.getElementById("Seven").addEventListener("click", reset,false);
+    document.getElementById("Eight").addEventListener("click", reset,false);
+    document.getElementById("Nine").addEventListener("click", reset,false);
+    document.getElementById("Four").addEventListener("click", reset,false);
+    document.getElementById("Five").addEventListener("click", reset,false);
+    document.getElementById("Six").addEventListener("click", reset,false);
+    document.getElementById("One").addEventListener("click", reset,false);
+    document.getElementById("Two").addEventListener("click", reset,false);
+    document.getElementById("Three").addEventListener("click", reset,false);
+    document.getElementById("Decimal").addEventListener("click", reset,false);
+    document.getElementById("Zero").addEventListener("click", reset,false);
+
+
+}
+
+window.addEventListener("load",start,false);
